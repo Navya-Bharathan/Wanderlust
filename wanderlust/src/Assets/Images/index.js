f3b5aa0cf75cbd1fe54c19e8d3ba242c38cf0ad0
Navya Bharathan <navyabharathan@Navyas-MacBook-Pro.local>
2023-05-27 17:07:25 +0200

    UI for home page

---
+++ wanderlust/src/Assets/Images/index.js
@@ -0,0 +1,85 @@
+
+export const FeaturedHotels=[
+    {
+        id:1,
+        FHImage:require('./FH1.jpg'),
+        title:"Am Irrenbachle",
+        rating:8.5,
+        place:"Peru",
+        country:"Chile",
+        price:"100Euro/per night"
+    },
+    {
+        id:2,
+        FHImage:require('./FH2.jpg'),
+        title :"Aparthotel Stare Miasto",
+        rating:8.3,
+        place:"madrid",
+        country:"Spain",
+        price:"120Euro/per night"
+    },
+    {
+        id:3,
+        FHImage:require('./FH3.jpg'),
+        title:"Gasthof Bergquelle",
+        rating:8.5,
+        place:"Bavaria",
+        country:"Germany",
+        price:"130Euro/per night"
+    },
+    {
+        id:4,
+        FHImage:require('./FH4.jpg'),
+        title :"Hotel Barnimer Hof",
+        rating:8.3,
+        place:"Wandalitz",
+        country:"Germany",
+        price:"90Euro/per night"
+    }
+
+
+
+]
+export const TopDestinations=[
+    {
+        id:1,
+        title:"New York",
+        TDImage:require('./TD1-newyork.jpg'),
+        hotels:"120+"
+    },
+    {
+        id:2,
+        title:"Sydney",
+        TDImage:require('./TD2-sydney.jpg'),
+        hotels:"135+"
+    },
+    {
+        id:1,
+        title:"New York",
+        TDImage:require('./TD3-tokyo.jpg'),
+        hotels:"100+"
+    }
+]
+export const TravelInspirations=[
+    {
+        id:1,
+        title:"Amazing Hotels on Beach",
+        TIImage:require('./Inspiration2.jpg'),
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+    },
+    {
+        id:2,
+        title:"Get close to nature",
+        TIImage:require('./Inspiration3.jpg'),
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+    },
+    {
+        id:3,
+        title:"Top Summer Destinations",
+        TIImage:require('./Inspiration1.jpg'),
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    
+    }
+]
