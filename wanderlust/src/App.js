e4e6bbbb776c22999fd09529bc34b390ff5accd0
Navya Bharathan <navyabharathan@Navyas-MacBook-Pro.local>
2023-06-10 12:09:39 +0200

    completed UI

--- wanderlust/src/App.js
+++ wanderlust/src/App.js
@@ -5,14 +5,18 @@ import Footer from './Components/Footer/Footer'
 import Home from './Pages/Home/Home'
 import AboutUs from './Pages/AboutUs/AboutUs';
 import Contact from './Pages/Contact/Contact';
+import List from './Pages/List/List';
+import Hotel from './Pages/Hotel/Hotel';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      
+
       <Routes>
         <Route path="/" element={<Home />}> </Route>
+        <Route path="/hotellist" element={<List />}> </Route>
+        <Route path="/hotel/:id" element={<Hotel />}> </Route>
         <Route path="/aboutus" element={< AboutUs/>}> </Route>
         <Route path="/contact" element={< Contact/>}> </Route>
       </Routes>
@@ -21,5 +25,5 @@ function App() {
     </div>
   );
 }
-
 export default App;
+
--- wanderlust/src/Data/data.js
+++ wanderlust/src/Data/data.js
@@ -15,3 +15,82 @@ export  const RecentPost=[
         date:"1/04/2023"
     }
 ]
+export const HotelList=[
+    {
+        id:1,
+        title:"Podere La Torre",
+        type:"Hotel",
+        location:"Madrid",
+        pincode:785643,
+        address:"Ajalvir",
+        country:"Spain",
+        image:require("../Assets/Images/FH1.jpg"),
+        rating:4,
+        distance:"4km",
+        attraction:"Lake",
+        offer:"Book stay over for 3 days at this property and get a free airport taxi",
+        shortDesc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        
+    },
+    {
+        id:2,
+        title:"Podere La Torre",
+        type:"Hotel",
+        location:"Madrid",
+        pincode:785643,
+        address:"Ajalvir",
+        country:"Spain",
+        image:require("../Assets/Images/FH2.jpg"),
+        rating:5,
+        distance:"2km",
+        attraction:"Airport",
+        offer:" free airport taxi",
+        shortDesc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    
+    },
+    {
+        id:3,
+        title:"Podere La Torre",
+        type:"Hotel",
+        location:"Madrid",
+        pincode:785643,
+        address:"Ajalvir",
+        country:"Spain",
+        image:require("../Assets/Images/FH3.jpg"),
+        rating:2,
+        distance:"2km",
+        attraction:"Airport",
+        offer:" free airport taxi",
+        shortDesc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    
+    }
+]
+export const HotelImages=[
+    {
+        id:1,
+     image:require("../Assets/Images/FH1.jpg")
+    },
+    {
+        id:2,
+     image:require("../Assets/Images/FH2.jpg")
+    },
+    {
+        id:3,
+     image:require("../Assets/Images/FH3.jpg")
+    },
+    {
+        id:4,
+     image:require("../Assets/Images/FH4.jpg")
+    },
+    {
+        id:5,
+     image:require("../Assets/Images/FH3.jpg")
+    },
+    {
+        id:6,
+     image:require("../Assets/Images/FH2.jpg")
+    }
+]
